8441c17d5206f58a5a31e3ea634b67afb2fc088e
sakura2317 <blackbutterfly.0929@gmail.com>
2015-07-22 11:34:44 +0900

    追加

--- Visual Studio/HelloWord/p3-6 Dice/Dice.cs
+++ Visual Studio/HelloWord/p3-6 Dice/Dice.cs
@@ -8,11 +8,61 @@ namespace p3_6
 {
     class Dice
     {
-        public int Roll()
+        private int[] stats;
+        private List<int> history;
+        private int total;
+
+        Random r = new Random();
+
+
+        public Dice()
         {
-            Random r = new Random();
-            return r.Next(1,7);
+            stats = new int[7] { 0, 0, 0, 0, 0, 0, 0 };
+            history = new List<int>();
         }
 
+        public int Roll()
+        {
+            //サイコロをふる
+            int result = r.Next(1,7);
+            //履歴に保存
+            history.Add(result);
+            //統計配列に保存
+            stats[result]++;
+            //結果を返す
+            return result;
+        }
+
+        //統計情報を出す
+        public string getStats()
+        {
+            int sum = 0;
+            for (int i = 1; i <= 6; i++)
+            {
+                int ratio = stats[i] * 100 / total;
+                msg += i + ":" + stats[i] + "(" + "%)\n";
+                sum += stats[i] * i;
+            }
+            msg += "合計：" + total + "回\n";
+            msg += "平均：" + ((double)sum / total) + "\n";
+
+            return "";
+        }
+        //履歴情報を出す
+        public string getHistory()
+        {
+            foreach (var item in history)
+            {
+                msg += "" + item;
+            }
+            msg += "\n";
+        }
+
+        history.Add(result);
+            stats[result]++;
+            total++;
+
+            return "";
+        }
     }
 }
--- Visual Studio/HelloWord/p3-6 Dice/Program.cs
+++ Visual Studio/HelloWord/p3-6 Dice/Program.cs
@@ -10,11 +10,7 @@ namespace p3_6
     {
         static void Main(string[] args)
         {
-            var dice    = new Dice();
-            var history = new List<int>();
-            var stats   = new int[7] {0,0,0,0,0,0,0};
-            var total = 0;
-
+            var dice = new Dice[3];
 
             while (true)
             {
@@ -22,28 +18,14 @@ namespace p3_6
                 string msg = "";
                 if (input == "h")
                 {
-                    msg = "履歴：";
-                    foreach (var item in history)
+                    for ()
                     {
-                        msg += "" + item;
+                        msg = dice[i].getHistory();
+                        msg += dice[i].getStats();
                     }
-                    msg += "\n";
-                    int sum = 0;
-                    for(int i = 1; i <= 6; i++)
-                    {
-                        int ratio = stats[i] * 100 / total;
-                        msg += i + ":" + stats[i] + "(" + "%)\n";
-                        sum += stats[i] * i;
-                    }
-                    msg += "合計：" + total + "回\n";
-                    msg += "平均：" + ((double)sum/total) + "\n";
-                }
                 else
                 {
                     int result = dice.Roll();
-                    history.Add(result);
-                    stats[result]++;
-                    total++;
                     msg = result.ToString();
 
                 }
